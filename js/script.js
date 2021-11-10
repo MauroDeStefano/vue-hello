f33459859886790cf1838a8bee3bf3155972ca2d
MauroDeStefano <maurodestefano@gmail.com>
2021-11-10 14:29:10 +0100

    first and last commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    message :"Buongiorno mondo",
+    img: "https://www.promoplanet.it/wp-content/uploads/2017/03/mondo.png"
+  },
+  method: {
+    
+  }
+
+});
